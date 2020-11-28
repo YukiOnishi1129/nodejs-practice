d932473c007e48cfc71a3362d6c589816ff270f4
大西雄貴 <yuki.onishi1129@gmail.com>
2020-11-28 22:06:22 +0900

    ルーティング

--- node-app/template.js
+++ node-app/template.js
@@ -2,18 +2,35 @@ const http = require('http');
 const fs = require('fs');
 // ejsオブジェクトの読み込み
 const ejs = require('ejs');
+// urlオブジェクトの読み込み
+const url = require('url');
 // テンプレートファイルの読み込み
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
+const style_css = fs.readFileSync('./style.css', 'utf8');
 
 const getFromClient = (request, response) => {
-  // レンダリングの実行
-  var content = ejs.render(index_page, {
-    title: 'Indexページ',
-    content: 'これはテンプレートを使ったサンプルページです。',
-  });
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(content);
-  response.end();
+  var url_parts = url.parse(request.url);
+  switch (url_parts.pathname) {
+    case '/':
+      // レンダリングの実行
+      var content = ejs.render(index_page, {
+        title: 'Indexページ',
+        content: 'これはテンプレートを使ったサンプルページです。',
+      });
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write(content);
+      response.end();
+      break;
+    case '/style.css':
+      response.writeHead(200, { 'Content-Type': 'text/css' });
+      response.write(style_css);
+      response.end();
+      break;
+    default:
+      response.writeHead(200, { 'Content-Type': 'text/plain' });
+      response.end('no page...');
+      break;
+  }
 };
 
 var server = http.createServer(getFromClient);
