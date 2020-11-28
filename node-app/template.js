994d962274c7eb1726913ef6f90891ef00f8bd5b
大西雄貴 <yuki.onishi1129@gmail.com>
2020-11-28 23:24:20 +0900

    POST送信

--- node-app/template.js
+++ node-app/template.js
@@ -4,39 +4,87 @@ const fs = require('fs');
 const ejs = require('ejs');
 // urlオブジェクトの読み込み
 const url = require('url');
+const qs = require('querystring');
 // テンプレートファイルの読み込み
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
 const other_page = fs.readFileSync('./other.ejs', 'utf8');
 const style_css = fs.readFileSync('./style.css', 'utf8');
 
+/**
+ * indexのアクセス処理
+ * @param {*} request
+ * @param {*} response
+ */
+const response_index = (request, response) => {
+  var msg = 'これはIndexページです。';
+  var content = ejs.render(index_page, {
+    title: 'Index',
+    content: msg,
+  });
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(content);
+  response.end();
+};
+
+/**
+ * otherのアクセス処理
+ * @param {*} request
+ * @param {*} response
+ */
+const response_other = (request, response) => {
+  var msg = 'これはOtherページです。';
+
+  // POSTアクセス処理
+  if (request.method == 'POST') {
+    var body = '';
+
+    // データ受診のイベント処理
+    // data: クライアントからデータを受け取ると発生するイベント
+    request.on('data', (data) => {
+      body += data;
+    });
+
+    // データ受診終了のイベント処理
+    // end: データの受け取りが完了したら発生するイベント
+    request.on('end', () => {
+      // qs.parseでデータをエンコードし、オブジェクトに変換
+      var post_data = qs.parse(body);
+      msg += 'あなたは、「' + post_data.msg + '」と書きました。';
+      var content = ejs.render(other_page, {
+        title: 'Other',
+        content: msg,
+      });
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.write(content);
+      response.end();
+    });
+  } else {
+    var msg = 'ページがありません。';
+    var content = ejs.render(other_page, {
+      title: 'Other',
+      content: msg,
+    });
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(content);
+    response.end();
+  }
+};
+
+/**
+ * createServerの処理
+ * @param {*} request
+ * @param {*} response
+ */
 const getFromClient = (request, response) => {
   // 第２引数にtrueをつけると、クエリーパラメータもパースされる
   var url_parts = url.parse(request.url, true);
   switch (url_parts.pathname) {
     case '/':
-      var subscribe = 'これはIndexページです。';
-      var query = url_parts.query;
-      if (query.msg != undefined) {
-        subscribe += 'あなたは、「' + query.msg + '」お送りしました。';
-      }
-      // レンダリングの実行
-      var content = ejs.render(index_page, {
-        title: 'Index',
-        content: subscribe,
-      });
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.write(content);
-      response.end();
+      response_index(request, response);
       break;
 
     case '/other':
-      var content = ejs.render(other_page, {
-        title: 'Other',
-        content: 'これはOtherページです。',
-      });
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.write(content);
-      response.end();
+      response_other(request, response);
       break;
 
     case '/style.css':
