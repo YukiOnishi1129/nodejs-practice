7e3baa54b86a18a194e05e4232331774feff53c1
大西雄貴 <yuki.onishi1129@gmail.com>
2020-11-30 00:19:28 +0900

    XMLデータの取り扱い

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const http = require('https');
+const parseString = require('xml2js').parseString;
 
 // 第一引数に'/'を指定する理由
 // → app.jsで指定した'/hello'の後に続くパスを示す
@@ -7,16 +9,38 @@ router.get('/', (req, res, next) => {
   // GETのクエリーパラメータを取得
   //   var name = req.query.name;
   //   var mail = req.query.mail;
-  var msg = '※何か書いて送信して下さい。';
-  //   sessionの値をセット
-  if (req.session.message != undefined) {
-    msg = 'Last Message: ' + req.session.message;
-  }
-  var data = {
-    title: 'Hello!',
-    content: msg,
+  //   var msg = '※何か書いて送信して下さい。';
+  //   //   sessionの値をセット
+  //   if (req.session.message != undefined) {
+  //     msg = 'Last Message: ' + req.session.message;
+  //   }
+  //   var data = {
+  //     title: 'Hello!',
+  //     content: msg,
+  //   };
+  var opt = {
+    host: 'news.google.com',
+    port: 443, //HTTPSの場合は443
+    path: '/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja',
   };
-  res.render('hello', data);
+  // サイトへアクセス
+  http.get(opt, (res2) => {
+    var body = '';
+    res2.on('data', (data) => {
+      body += data;
+    });
+    res2.on('end', () => {
+      // XMLデータをパースする(変換)
+      parseString(body.trim(), (err, result) => {
+        console.log(result);
+        var data = {
+          title: 'Google News',
+          content: result.rss.channel[0].item,
+        };
+        res.render('hello', data);
+      });
+    });
+  });
 });
 
 router.post('/post', (req, res, next) => {
