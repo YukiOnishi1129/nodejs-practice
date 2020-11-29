3115c6ef0a1fd764f03e1f2565877d1a6354cb92
大西雄貴 <yuki.onishi1129@gmail.com>
2020-11-29 23:39:59 +0900

    get. post

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -4,9 +4,22 @@ const router = express.Router();
 // 第一引数に'/'を指定する理由
 // → app.jsで指定した'/hello'の後に続くパスを示す
 router.get('/', (req, res, next) => {
+  // GETのクエリーパラメータを取得
+  //   var name = req.query.name;
+  //   var mail = req.query.mail;
   var data = {
     title: 'Hello!',
-    content: 'これは、サンプルのコンテンツです。<br>this is sample content.',
+    content: '※何か書いて送信して下さい。',
+  };
+  res.render('hello', data);
+});
+
+router.post('/post', (req, res, next) => {
+  // POST送信された値を取得する
+  var msg = req.body['message'];
+  var data = {
+    title: 'Hello!',
+    content: 'あなたは、「' + msg + '」と送信しました。',
   };
   res.render('hello', data);
 });
