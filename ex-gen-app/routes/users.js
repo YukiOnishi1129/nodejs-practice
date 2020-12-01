6d2c7a7802b62473b206eef6da920d8d65140562
大西雄貴 <yuki.onishi1129@gmail.com>
2020-12-01 21:02:14 +0900

    検索

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models/index');
+const { Op } = require('sequelize');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  const min = req.query.min * 1;
+  const max = req.query.max * 1;
+  db.User.findAll({
+    where: {
+      age: { [Op.gte]: min, [Op.lte]: max },
+    },
+  }).then((users) => {
+    var data = {
+      title: 'Users/Index',
+      content: users,
+    };
+    res.render('users/index', data);
+  });
 });
 
 module.exports = router;
