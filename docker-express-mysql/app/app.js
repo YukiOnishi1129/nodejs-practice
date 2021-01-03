85dd09084a1615218a68555308c19e6f473f9c0c
yuki <yuki.onishi1129@gmail.com>
2021-01-04 01:21:44 +0900

    select処理

--- docker-express-mysql/app/app.js
+++ docker-express-mysql/app/app.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -8,15 +9,27 @@ const con = mysql.createConnection({
   host: "db", // Docker上ではlocalhostに接続してはいけない
   user: "root",
   password: "pass",
+  database: "DB_EXPRESS",
 });
 
 con.connect((err) => {
-  if (err) {
-    throw err;
-  }
+  if (err) throw err;
   console.log("Connected");
+  //  table作成
+  // const sql =
+  //   "CREATE TABLE IF NOT EXISTS users (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL)";
+  // con.query(sql, (err, result) => {
+  //   if (err) throw err;
+  //   console.log("table created");
+  // });
 });
 
-app.get("/", (req, res) => res.send("Hello, World!"));
+app.get("/", (req, res) => {
+  const sql = "select * from users";
+  con.query(sql, (err, result, fields) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
