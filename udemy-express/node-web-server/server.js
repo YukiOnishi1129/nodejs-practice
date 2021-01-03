1975a925bdb9928872f02003480ead4376ad462b
yuki <yuki.onishi1129@gmail.com>
2021-01-03 17:22:04 +0900

    add: prettier

--- udemy-express/node-web-server/server.js
+++ udemy-express/node-web-server/server.js
@@ -3,9 +3,8 @@ const express = require('express')
 // app変数でwebサーバー機能を編集する
 const app = express()
 
-app.get("/", (req, res) => {
-    res.send("Hello, Express!")
+app.get('/', (req, res) => {
+  res.send('Hello, Express!')
 })
 
 app.listen(3000)
-
