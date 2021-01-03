3918f549281bb93b8f3e799897c4c228a21e88df
yuki <yuki.onishi1129@gmail.com>
2021-01-03 17:27:05 +0900

    study: routing

--- udemy-express/node-web-server/server.js
+++ udemy-express/node-web-server/server.js
@@ -4,7 +4,16 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express!')
+  // HTMLを送信できる
+  res.send('<h1>Hello, Express!</h1>')
+})
+
+app.get('/about', (req, res) => {
+  // jsonを送信できる
+  res.send({
+    name: '太郎',
+    age: 20,
+  })
 })
 
 app.listen(3000)
