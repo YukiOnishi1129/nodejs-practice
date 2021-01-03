880cd41a1f53a04429e6cc65b5e78baef761fab5
yuki <yuki.onishi1129@gmail.com>
2021-01-04 01:07:04 +0900

    docker上のnode.jsより、mysqlに接続成功

--- docker-express-mysql/app/app.js
+++ docker-express-mysql/app/app.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const mysql = require("mysql");
+
+const con = mysql.createConnection({
+  host: "db", // Docker上ではlocalhostに接続してはいけない
+  user: "root",
+  password: "pass",
+});
+
+con.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected");
+});
+
 app.get("/", (req, res) => res.send("Hello, World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
