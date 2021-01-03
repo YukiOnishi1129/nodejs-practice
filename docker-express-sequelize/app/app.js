9abd7dd9bd7a3ecd32c6e26d0e8303ca851f139b
yuki <yuki.onishi1129@gmail.com>
2021-01-04 02:02:52 +0900

    下準備

---
+++ docker-express-sequelize/app/app.js
@@ -0,0 +1,35 @@
+const { query } = require("express");
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const port = 3000;
+
+const mysql = require("mysql");
+
+// テンプレートエンジン追加
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const con = mysql.createConnection({
+  host: "db", // Docker上ではlocalhostに接続してはいけない
+  user: "root",
+  password: "pass",
+  database: "DB_EXPRESS",
+});
+
+con.connect((err) => {
+  if (err) throw err;
+  console.log("Connected");
+});
+
+/**
+ * select処理
+ */
+app.get("/", (req, res) => {
+  res.send("こんにちは");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
