a18a22a7c457910e3c5a2c2eb8180e58a3d15d43
yuki <yuki.onishi1129@gmail.com>
2021-01-03 20:39:51 +0900

    study: post API

--- udemy-express/express-demo/index.js
+++ udemy-express/express-demo/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const Joi = require('joi')
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const courses = [
   { id: 1, name: 'course1' },
@@ -12,9 +17,31 @@ app.get('/', (req, res) => {
   res.send('Hello, World!')
 })
 
-// app.get('/courses', (req, res) => {
-//   res.send(courses)
-// })
+app.get('/courses', (req, res) => {
+  res.send(courses)
+})
+
+app.post('/courses', (req, res) => {
+  //   if (!req.body.name || req.body.length < 3) {
+  //     // res.send('入力必須、かつ3文字以上')
+  //     res.status(400)
+  //   }
+  // Joiを使ったバリデーション
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  })
+  let result = schema.validate(req.body)
+  if (result.error) {
+    res.send(result.error.details[0].message)
+  }
+
+  let course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  }
+  courses.push(course)
+  res.send(courses)
+})
 
 app.get('/courses/:id', (req, res) => {
   let course = courses.find((e) => e.id === parseInt(req.params.id))
