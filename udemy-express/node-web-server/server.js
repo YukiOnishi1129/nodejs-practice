1ecd57b12fe075abe92778f14ea61b1c7796cade
yuki <yuki.onishi1129@gmail.com>
2021-01-03 17:03:24 +0900

    app変数

---
+++ udemy-express/node-web-server/server.js
@@ -0,0 +1,11 @@
+// サーバーとして機能する
+const express = require('express')
+// app変数でwebサーバー機能を編集する
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello, Express!")
+})
+
+app.listen(3000)
+
