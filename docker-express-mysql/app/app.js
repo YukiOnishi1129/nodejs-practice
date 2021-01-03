a53a2ef444867dcf40416bd570307a0893544770
yuki <yuki.onishi1129@gmail.com>
2021-01-04 01:38:27 +0900

    insert処理作成

--- docker-express-mysql/app/app.js
+++ docker-express-mysql/app/app.js
@@ -1,9 +1,13 @@
 const { query } = require("express");
 const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
 const mysql = require("mysql");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const con = mysql.createConnection({
   host: "db", // Docker上ではlocalhostに接続してはいけない
@@ -24,11 +28,39 @@ con.connect((err) => {
   // });
 });
 
+/**
+ * select処理
+ */
 app.get("/", (req, res) => {
   const sql = "select * from users";
   con.query(sql, (err, result, fields) => {
     if (err) throw err;
     res.send(result);
+  });
+});
+
+// app.get("/insert", (req, res) => {
+//   const sql = "INSERT INTO users(name,email) VALUES(?,?)";
+//   con.query(sql, ["Jack", "jack@exsample.co.jp"], (err, result, fields) => {
+//     if (err) throw err;
+//     res.send(result);
+//   });
+// });
+
+app.get("/form", (req, res) => {
+  res.sendFile(path.join(__dirname, "html/form.html"));
+});
+
+/**
+ * insert処理
+ */
+app.post("/form", (req, res) => {
+  const sql = "INSERT INTO users SET ?";
+  con.query(sql, req.body, (err, result, fields) => {
+    if (err) throw err;
+    // console.log(result);
+    // res.send("登録が完了しました");
+    res.redirect("/");
   });
 });
 
