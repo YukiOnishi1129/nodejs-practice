47c08ca19b5516c91eabb18b89ece7c29ce725db
yuki <yuki.onishi1129@gmail.com>
2021-01-04 23:09:07 +0900

    sequelizeにてget APIを作成

--- docker-express-sequelize/app/app.js
+++ docker-express-sequelize/app/app.js
@@ -1,32 +1,13 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const db = require("./models/index"); // cliでinitした時に作成されるmodels配下のindex.js
 const app = express();
 const port = 3000;
 
-const mysql = require("mysql");
-
-// テンプレートエンジン追加
-app.set("view engine", "ejs");
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const con = mysql.createConnection({
-  host: "db", // Docker上ではlocalhostに接続してはいけない
-  user: "root",
-  password: "pass",
-  database: "DB_EXPRESS",
-});
-
-con.connect((err) => {
-  if (err) throw err;
-  console.log("Connected");
-});
-
-/**
- * select処理
- */
-app.get("/", (req, res) => {
-  res.send("こんにちは");
+app.get("/users", (req, res) => {
+  db.user.findAll({}).then((instances) => {
+    console.log(instances);
+    res.send(instances);
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
